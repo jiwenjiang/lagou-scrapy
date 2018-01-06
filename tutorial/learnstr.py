7e2f82b73e01334846e4ea7bc3a8e02c0fcbdadc
513755565@qq.com <513755565@qq.com>
2018-01-06 19:49:19 +0800

    test static method, class method, obj method

--- tutorial/learnstr.py
+++ tutorial/learnstr.py
@@ -1,3 +1,29 @@
-print(type(()))
-print(max('12asd'))
-print({1, 2, 3, 4} | {2, 3, 5})
+# print(type(()))
+# print(max('12asd'))
+# print({1, 2, 3, 4} | {2, 3, 5})
+# l = [1, 2, 4, 6, 5, 6, 7, 8]
+#
+# for i in range(0, len(l), 2):
+#     print(l[i])
+import tutorial
+
+tutorial.sys.setrecursionlimit(500)
+
+
+class Student():
+    name = 'aa'
+    ff = 2
+
+    def __init__(self, name):
+        self.name = name
+        print(name)
+        pass
+
+    @classmethod
+    def printname(cls):
+        cls.ff += 1
+        print(cls.ff)
+
+
+stu = Student('ff333')
+Student.printname()
