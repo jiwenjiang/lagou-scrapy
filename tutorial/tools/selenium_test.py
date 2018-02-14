977d54886e62eae33e0509cdf6b00873e4c1cdf8
513755565@qq.com <513755565@qq.com>
2018-02-14 17:04:43 +0800

    模拟知乎登陆

--- tutorial/tools/selenium_test.py
+++ tutorial/tools/selenium_test.py
@@ -2,6 +2,20 @@ from selenium import webdriver
 
 browser = webdriver.Chrome(executable_path="E:\JDK\selenium\chromedriver.exe")
 
-browser.get("https://jiwenjiang.github.io/#/login")
+browser.get("https://www.zhihu.com/signup?next=%2F")
 
-print(browser.page_source)
+
+def click_ele(c):
+    browser.find_element_by_css_selector(c).click()
+
+
+def select_ele(c, k):
+    browser.find_element_by_css_selector(c).send_keys(k)
+
+
+click_ele(".SignContainer-switch span")
+select_ele(".SignFlow-accountInput input[name='username']", "18380464714")
+select_ele(".SignFlow-password input[name='password']", "xxxxxxxxxxxxxxx")
+click_ele("button.SignFlow-submitButton")
+
+# print(browser.page_source)
