cca1520e3d6e773577ef471bb1ed6e3e17e20651
513755565@qq.com <513755565@qq.com>
2018-02-25 14:07:21 +0800

    load image

--- tutorial/tools/selenium_test.py
+++ tutorial/tools/selenium_test.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 import time
 
-browser = webdriver.Chrome(executable_path="E:\JDK\selenium\chromedriver.exe")
+# loadimage
+chrome_opt = webdriver.ChromeOptions()
+prefs = {"profile.managed_default_content_settings.images": 2}
+chrome_opt.add_experimental_option("prefs", prefs)
+# loadimage
+
+
+browser = webdriver.Chrome(executable_path="E:\JDK\selenium\chromedriver.exe", chrome_options=chrome_opt)
 
 browser.get("https://www.zhihu.com/signup?next=%2F")
 
@@ -16,12 +23,12 @@ def select_ele(c, k):
 
 click_ele(".SignContainer-switch span")
 select_ele(".SignFlow-accountInput input[name='username']", "18380464714")
-select_ele(".SignFlow-password input[name='password']", "xxxxx")
+select_ele(".SignFlow-password input[name='password']", "bleachsilence4.")
 click_ele("button.SignFlow-submitButton")
 time.sleep(3)
 for i in range(3):
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight); var num=document.body.scrollHeight; return num;")
+    browser.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight); var num=document.body.scrollHeight; return num;")
     time.sleep(3)
-
 
 # print(browser.page_source)
